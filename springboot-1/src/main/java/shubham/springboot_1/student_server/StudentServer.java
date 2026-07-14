da4ae5a814e284e8a30dea17444e77e208c7e8e7
Staggered95 <shubhamsinghup720@gmail.com>
2026-07-14 12:56:51 +0530

    POST with student

--- springboot-1/src/main/java/shubham/springboot_1/student_server/StudentServer.java
+++ springboot-1/src/main/java/shubham/springboot_1/student_server/StudentServer.java
@@ -1,14 +1,20 @@
 package shubham.springboot_1.student_server;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentServer {
     //CREATE
     @PostMapping("/student")
-    public String storeStudent() {
-        return "Student information stored";
+    public String storeStudent(@RequestBody Student student) {
+        int id = student.getId();
+        int age = student.getAge();
+        String name = student.getName();
+        String department = student.getDepartment();
+
+        return "Name: " + name + " ID: " + id + " Age: " + age + " Department: " + department;
     }
 
     //READ

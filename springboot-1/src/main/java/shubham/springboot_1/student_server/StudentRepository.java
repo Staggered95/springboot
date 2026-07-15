a5cc526f7dee5ab19fd7a38fcca110a927d4c1a9
Staggered95 <shubhamsinghup720@gmail.com>
2026-07-15 11:27:21 +0530

    data JPA

--- springboot-1/src/main/java/shubham/springboot_1/student_server/StudentRepository.java
+++ springboot-1/src/main/java/shubham/springboot_1/student_server/StudentRepository.java
@@ -1,11 +1,12 @@
 package shubham.springboot_1.student_server;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class StudentRepository {
-    public Student save(Student student) {
-        System.out.println("Student info saved");
-        return student;
-    }
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+//    public Student save(Student student) {
+//        System.out.println("Student info saved");
+//        return student;
+//    }
 }

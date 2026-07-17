d6debf2d50330b1c0d02a157b5843faf9308268b
Staggered95 <shubhamsinghup720@gmail.com>
2026-07-17 15:51:17 +0530

    Added: minimal ui for crud implementation. static files served by springboot

---
+++ springboot-1/src/main/resources/static/script.js
@@ -0,0 +1,78 @@
+const API = "/student";
+
+async function loadStudents() {
+
+    const response = await fetch(API);
+    const students = await response.json();
+
+    const tbody = document.getElementById("students");
+    tbody.innerHTML = "";
+
+    students.forEach(student => {
+
+        tbody.innerHTML += `
+        <tr>
+            <td>${student.id}</td>
+            <td>${student.name}</td>
+            <td>${student.age}</td>
+            <td>${student.department}</td>
+            <td>
+                <button onclick="deleteStudent(${student.id})">
+                    Delete
+                </button>
+            </td>
+        </tr>`;
+    });
+}
+
+async function createStudent() {
+
+    const student = {
+        id: Number(document.getElementById("id").value),
+        name: document.getElementById("name").value,
+        age: Number(document.getElementById("age").value),
+        department: document.getElementById("department").value
+    };
+
+    await fetch(API,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(student)
+    });
+
+    loadStudents();
+}
+
+async function updateStudent() {
+
+    const id = document.getElementById("id").value;
+
+    const student = {
+        name: document.getElementById("name").value,
+        age: Number(document.getElementById("age").value),
+        department: document.getElementById("department").value
+    };
+
+    await fetch(`${API}/${id}`,{
+        method:"PUT",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(student)
+    });
+
+    loadStudents();
+}
+
+async function deleteStudent(id){
+
+    await fetch(`${API}/${id}`,{
+        method:"DELETE"
+    });
+
+    loadStudents();
+}
+
+loadStudents();
